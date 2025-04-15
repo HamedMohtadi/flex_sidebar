99d78a2dabeaaaf99d5532a9402bfd271751d666
orca741 <hamedm@chmail.ir>
2025-04-15 15:39:27 +0430

    - minor progress

--- example/lib/in_stack_example.dart
+++ example/lib/in_stack_example.dart
@@ -18,18 +18,18 @@ class _StackExampleState extends State<StackExample> {
       child: Stack(
         children: [
           Scaffold(
-              appBar: AppBar(
-                title: Text(
-                  "Title",
-                  style: const TextStyle(
-                      fontFamily: 'Yekan',
-                      fontSize: 32,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.blueGrey),
-                ),
-                centerTitle: true,
-              ),
-              body: SafeArea(child: Text("data"))),
+              body: SafeArea(
+                  child: Center(
+            child: Container(
+                color: Theme.of(context).primaryColor.withAlpha(100),
+                child: Center(
+                  child: Text(
+                    "Item 1",
+                    style: TextStyle(
+                        fontSize: 53, color: Theme.of(context).hintColor),
+                  ),
+                )),
+          ))),
           Positioned.directional(
             textDirection: Directionality.of(context),
             start: 0,
@@ -56,9 +56,30 @@ class _StackExampleState extends State<StackExample> {
                 ],
                 primaryWidget: Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: Image.asset('assets/images/person.png'),
+                  child: SizedBox(
+                      width: 200,
+                      child: Image.asset('assets/images/person.png')),
                 ),
-                secondaryWidget: Text("data"),
+                secondaryWidget: Column(
+                  children: [
+                    Text(
+                      "title",
+                      style: TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black54),
+                    ),
+                    Text(
+                      "This can be a description",
+                      style: TextStyle(color: Theme.of(context).dividerColor),
+                    ),
+                    Text(
+                      "another description!",
+                      style: TextStyle(
+                          color: Theme.of(context).dividerColor.withAlpha(200)),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
